6d0eea223c5003bd7ddf9782cad9e5239d476be3
ALI TAYYAB <01-134232-037@student.bahria.edu.pk>
2025-07-16 09:31:10 +0500

    .js was missing

--- src/routes/message.routes.js
+++ src/routes/message.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { verifyJWT } from "../middlewares/auth.middleware";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { getUsers, getMessages } from "../controllers/message.controller.js";
 const router = Router();
 
