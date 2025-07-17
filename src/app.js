62cc614205af241817edb08fd519c0db0588ecc6
ALI TAYYAB <01-134232-037@student.bahria.edu.pk>
2025-07-17 08:50:11 +0500

    Configure CORS middleware with specific options for enhanced security

--- src/app.js
+++ src/app.js
@@ -5,7 +5,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
