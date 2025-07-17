d4728e893d447e4ce1be6ac627e277bb763ca933
ALI TAYYAB <01-134232-037@student.bahria.edu.pk>
2025-07-18 00:53:59 +0500

    FRONTEND DEPLOYED URLS

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.FRONT_END_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   },
@@ -21,7 +21,8 @@ const io = new Server(server, {
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONT_END_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
